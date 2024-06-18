2196b172143380d950a74fe20bb13c2291e37068
Coder6pack <105229432+Coder6pack@users.noreply.github.com>
2024-06-18 17:26:29 +0700

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <Button className='bg-red-500' variant={'outline'}>
+        Click me
+      </Button>
+    </>
+  )
+}
+
+export default App
