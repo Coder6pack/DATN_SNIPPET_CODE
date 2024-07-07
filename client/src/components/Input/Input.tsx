03b175748eb24219a039e35b39735ecc8c0e29be
Coder6pack <105229432+Coder6pack@users.noreply.github.com>
2024-07-08 01:45:17 +0700

    update 8/7/2024 UI user profile

---
+++ client/src/components/Input/Input.tsx
@@ -0,0 +1,29 @@
+import { InputHTMLAttributes } from 'react'
+import { RegisterOptions, UseFormRegister } from 'react-hook-form'
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  errorMessage?: string
+  autoComplete?: string
+  classNameInput?: string
+  classNameError?: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  register?: UseFormRegister<any>
+  rules?: RegisterOptions
+}
+export default function Input({
+  name,
+  className,
+  errorMessage,
+  register,
+  rules,
+  classNameInput = 'p-3 rounded-sm border border-gray-300 focus:border-gray-500 focus:shadow-sm w-full outline-none',
+  classNameError = 'mt-1 min-h-[1rem] text-sm text-red-600',
+  ...rest
+}: Props) {
+  const registerResult = register && name ? register(name, rules) : {}
+  return (
+    <div className={className}>
+      <input className={classNameInput} {...registerResult} {...rest} />
+      <div className={classNameError}>{errorMessage}</div>
+    </div>
+  )
+}
