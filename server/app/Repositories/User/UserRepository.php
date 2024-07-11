226baca604fbd96011c5387caa6149affca682b6
TZuynh <nguyentuanquynh101203@gmail.com>
2024-07-11 22:54:58 +0700

    register user, 11/7

--- server/app/Repositories/User/UserRepository.php
+++ server/app/Repositories/User/UserRepository.php
@@ -7,6 +7,7 @@ use App\Repositories\BaseRepository;
 use App\Repositories\User\UserRepositoryInterface;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Hash;
 
 class UserRepository extends BaseRepository implements UserRepositoryInterface
 {
@@ -41,5 +42,15 @@ class UserRepository extends BaseRepository implements UserRepositoryInterface
         $users = $this->model->where('role_id', $role_id)->where('img_id', $img_id)->first();
 
         return $this->destroy($users);
+    }
+
+    public function create($data)
+    {
+        if (isset($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
+        $data['role_id'] = 2;
+
+        return $this->model->create($data);
     }
 }
