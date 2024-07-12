5977504d809a6c03704d6dc7c88074a8702d7a32
TZuynh <nguyentuanquynh101203@gmail.com>
2024-07-12 15:32:23 +0700

    fix 12/7

--- server/app/Models/SnippetTag.php
+++ server/app/Models/SnippetTag.php
@@ -4,24 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class SnippetTag extends Model
 {
     use HasFactory, HasUlids;
 
-    protected $fillable = [
-        'snippet_id',
-        'name',
-    ];
-
-    /**
-     * 
-     * @return BelongsTo
-     */
-    public function snippet(): BelongsTo
-    {
-        return $this->belongsTo(Snippet::class);
-    }
+    protected $fillable = ['name',];
 }
